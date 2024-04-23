98d77162364ff7570af6d623c0b908ec512e3a6a
Parker Singleton <sps253@cornell.edu>
2024-04-23 16:20:12 -0400

    fig2_PCB

--- scripts/figure3_placeboSI.m
+++ scripts/figure3_placeboSI.m
@@ -78,46 +78,46 @@ rec_nosub = receptor_vec(1:100);
 
 [r2a,p2a] = corr(mean(diff_energy)',rec_nosub,'type','Spearman')
 
-p_spin_vasa = perm_sphere_p_al857(mean(diff_energy)',rec_nosub,perm_id,'Spearman')
+p_spin_vasa_left = perm_sphere_p_al857(mean(diff_energy)',rec_nosub,perm_id,'Spearman')
 
 figure;
 scatter(rec_nosub,mean(diff_energy)'); lsline
 xlabel('2a density')
 ylabel('pre/post PCB Nodal Control Energy (∆)')
-text(-.7,-0.5,['rho = ',num2str(r2a),'; p = ',num2str(p_spin_vasa)])
+text(-.7,0.15,['rho = ',num2str(r2a),'; p = ',num2str(p_spin_vasa_left)])
 
 
 %% dominance analysis - see figure4.m for plotting these data
-% load([basedir,'data/5HTvecs_sch116.mat'])
-% 
-% pre_energy = squeeze(mean(regional_CE_dmt_pre,2));
-% post_energy = squeeze(mean(regional_CE_dmt,2));
-% 
-% diff_energy = (post_energy-pre_energy)./(pre_energy);
-% 
-% v2a=mean5HT2A_sch116;
-% 
-% v1a=mean5HT1A_sch116;
-% 
-% v1b=mean5HT1B_sch116;
-% 
-% v4=mean5HT4_sch116;
-% 
-% vT=mean5HTT_sch116;
-% 
-% allv = [v2a v1a v1b v4 vT];
-% 
-% target = mean(diff_energy)';
-% 
-% varnames=[{'HT2a'},{'HT1a'},{'HT1b'},{'HT4'},{'HTT'},{'target'}];
-% 
-% mydata = table(v2a,v1a,v1b,v4,vT,target,'VariableNames',varnames);
-% 
-% input_filename = 'results/dominance/diffE_continuous';
-% writetable(mydata, [basedir,input_filename,note, '.csv']);
-% 
-% %call from terminal:
-% % python3 ~/Documents/GIT/DMT_NCT/fxns/dominance_analysis_al857.py ~/Documents/GIT/DMT_NCT/results/dominance/diffE_continuous_gsr_volnorm.csv  target 5 0 ~/Documents/GIT/DMT_NCT/results/dominance/diffE_continuous_gsr_volnorm
+load([basedir,'data/5HTvecs_sch116.mat'])
+
+pre_energy = squeeze(mean(regional_CE_pcb_pre,2));
+post_energy = squeeze(mean(regional_CE_pcb,2));
+
+diff_energy = (post_energy-pre_energy)./(pre_energy);
+
+v2a=mean5HT2A_sch116;
+
+v1a=mean5HT1A_sch116;
+
+v1b=mean5HT1B_sch116;
+
+v4=mean5HT4_sch116;
+
+vT=mean5HTT_sch116;
+
+allv = [v2a v1a v1b v4 vT];
+
+target = mean(diff_energy)';
+
+varnames=[{'HT2a'},{'HT1a'},{'HT1b'},{'HT4'},{'HTT'},{'target'}];
+
+mydata = table(v2a,v1a,v1b,v4,vT,target,'VariableNames',varnames);
+
+input_filename = 'results/dominance/diffE_pcb_continuous';
+writetable(mydata, [basedir,input_filename,note, '.csv']);
+
+%call from terminal:
+% python3 ~/Documents/GIT/DMT_NCT/fxns/dominance_analysis_al857.py ~/Documents/GIT/DMT_NCT/results/dominance/diffE_pcb_continuous_gsr_volnorm.csv  target 5 0 ~/Documents/GIT/DMT_NCT/results/dominance/diffE_pcb_continuous_gsr_volnorm
 
 %% regional CE vs intensity
 
@@ -201,42 +201,42 @@ rec_nosub = receptor_vec(1:100);
 [r2a,p2a] = corr(r_avg',rec_nosub,'type','Spearman')
 
 %permtest
-p_spin_vasa = perm_sphere_p_al857(r_avg',rec_nosub,perm_id,'Spearman')
+p_spin_vasa_right = perm_sphere_p_al857(r_avg',rec_nosub,perm_id,'Spearman')
 
 
 figure;
 scatter(rec_nosub,r_avg'); lsline
 xlabel('2a density')
 ylabel('Rho: Nodal correlation b/w CE and intensity')
-text(-.7,-0.8,['rho = ',num2str(r2a),'; p = ',num2str(p_spin_vasa)])
+text(-.7,0.1,['rho = ',num2str(r2a),'; p = ',num2str(p_spin_vasa_right)])
 
 %% dominance analysis - see figure4.m for plotting these data
-% load([basedir,'data/5HTvecs_sch116.mat'])
-% 
-% v2a=mean5HT2A_sch116;
-% 
-% v1a=mean5HT1A_sch116;
-% 
-% v1b=mean5HT1B_sch116;
-% 
-% v4=mean5HT4_sch116;
-% 
-% vT=mean5HTT_sch116;
-% 
-% allv = [v2a v1a v1b v4 vT];
-% 
-% [r_avg,p_avg]=corr(m_dmt_int',squeeze(mean(win_dmt_ce,1)));
-% target = r_avg';
-% 
-% varnames=[{'HT2a'},{'HT1a'},{'HT1b'},{'HT4'},{'HTT'},{'target'}];
-% 
-% mydata = table(v2a,v1a,v1b,v4,vT,target,'VariableNames',varnames);
-% 
-% input_filename = 'results/dominance/intcorr';
-% writetable(mydata, [basedir,input_filename,note, '.csv']);
-% 
-% %call from terminal:
-% % python3 ~/Documents/GIT/DMT_NCT/fxns/dominance_analysis_al857.py ~/Documents/GIT/DMT_NCT/results/dominance/intcorr_gsr_volnorm.csv  target 5 0 ~/Documents/GIT/DMT_NCT/results/dominance/intcorr_gsr_volnorm
+load([basedir,'data/5HTvecs_sch116.mat'])
+
+v2a=mean5HT2A_sch116;
+
+v1a=mean5HT1A_sch116;
+
+v1b=mean5HT1B_sch116;
+
+v4=mean5HT4_sch116;
+
+vT=mean5HTT_sch116;
+
+allv = [v2a v1a v1b v4 vT];
+
+[r_avg,p_avg]=corr(m_pcb_int',squeeze(mean(win_pcb_ce,1)));
+target = r_avg';
+
+varnames=[{'HT2a'},{'HT1a'},{'HT1b'},{'HT4'},{'HTT'},{'target'}];
+
+mydata = table(v2a,v1a,v1b,v4,vT,target,'VariableNames',varnames);
+
+input_filename = 'results/dominance/intcorr_pcb';
+writetable(mydata, [basedir,input_filename,note, '.csv']);
+
+%call from terminal:
+% python3 ~/Documents/GIT/DMT_NCT/fxns/dominance_analysis_al857.py ~/Documents/GIT/DMT_NCT/results/dominance/intcorr_pcb_gsr_volnorm.csv  target 5 0 ~/Documents/GIT/DMT_NCT/results/dominance/intcorr_pcb_gsr_volnorm
 
 
 %% SI FIGURE 4, middle correlate nodal CE with LZ
@@ -255,7 +255,7 @@ figure;
 scatter(receptor_vec,r_avg'); lsline
 xlabel('2a density')
 ylabel('Rho: Nodal correlation b/w CE and LZ')
-text(-2,-0.5,['rho = ',num2str(r2a),'; p = ',num2str(p2a)])
+text(-2,0.2,['rho = ',num2str(r2a),'; p = ',num2str(p2a)])
 
 %% FIGURE 3a, middle.  gummibrain from Keith Jamison-> https://github.com/kjamison/atlasblobs
 load(fullfile([basedir,'fxns/gummi_kj/atlasblobs_saved.mat']))
@@ -297,38 +297,43 @@ rec_nosub = receptor_vec(1:100);
 
 [r2a,p2a] = corr(r_avg',rec_nosub,'type','Spearman')
 
-p_spin_vasa = perm_sphere_p_al857(r_avg',rec_nosub,perm_id,'Spearman')
+p_spin_vasa_mid = perm_sphere_p_al857(r_avg',rec_nosub,perm_id,'Spearman')
 
 figure;
 scatter(rec_nosub,r_avg'); lsline
 xlabel('2a density')
 ylabel('Rho: Nodal correlation b/w CE and LZ')
-text(-.7,-0.5,['rho = ',num2str(r2a),'; p = ',num2str(p_spin_vasa)])
+text(-.7,0.1,['rho = ',num2str(r2a),'; p = ',num2str(p_spin_vasa_mid)])
 
 %% dominance analysis - see figure4.m for plotting these data
-% load([basedir,'data/5HTvecs_sch116.mat'])
-% 
-% v2a=mean5HT2A_sch116;
-% 
-% v1a=mean5HT1A_sch116;
-% 
-% v1b=mean5HT1B_sch116;
-% 
-% v4=mean5HT4_sch116;
-% 
-% vT=mean5HTT_sch116;
-% 
-% allv = [v2a v1a v1b v4 vT];
-% 
-% [r_avg,p_avg]=corr(m_dmt_LZ',squeeze(mean(regional_CE_dmt,1)));
-% target = r_avg';
-% 
-% varnames=[{'HT2a'},{'HT1a'},{'HT1b'},{'HT4'},{'HTT'},{'target'}];
-% 
-% mydata = table(v2a,v1a,v1b,v4,vT,target,'VariableNames',varnames);
-% 
-% input_filename = 'results/dominance/LZcorr';
-% writetable(mydata, [basedir,input_filename,note, '.csv']);
-% 
-% %call from terminal:
-% % python3 ~/Documents/GIT/DMT_NCT/fxns/dominance_analysis_al857.py ~/Documents/GIT/DMT_NCT/results/dominance/LZcorr_gsr_volnorm.csv  target 5 0 ~/Documents/GIT/DMT_NCT/results/dominance/LZcorr_gsr_volnorm
+load([basedir,'data/5HTvecs_sch116.mat'])
+
+v2a=mean5HT2A_sch116;
+
+v1a=mean5HT1A_sch116;
+
+v1b=mean5HT1B_sch116;
+
+v4=mean5HT4_sch116;
+
+vT=mean5HTT_sch116;
+
+allv = [v2a v1a v1b v4 vT];
+
+[r_avg,p_avg]=corr(m_pcb_LZ',squeeze(nanmean(regional_CE_pcb,1)));
+target = r_avg';
+
+varnames=[{'HT2a'},{'HT1a'},{'HT1b'},{'HT4'},{'HTT'},{'target'}];
+
+mydata = table(v2a,v1a,v1b,v4,vT,target,'VariableNames',varnames);
+
+input_filename = 'results/dominance/LZcorr_pcb';
+writetable(mydata, [basedir,input_filename,note, '.csv']);
+
+%call from terminal:
+% python3 ~/Documents/GIT/DMT_NCT/fxns/dominance_analysis_al857.py ~/Documents/GIT/DMT_NCT/results/dominance/LZcorr_pcb_gsr_volnorm.csv  target 5 0 ~/Documents/GIT/DMT_NCT/results/dominance/LZcorr_pcb_gsr_volnorm
+
+
+%% correct
+
+pfdr = mafdr([p_spin_vasa_left p_spin_vasa_mid p_spin_vasa_right],'BHFDR', 1)
